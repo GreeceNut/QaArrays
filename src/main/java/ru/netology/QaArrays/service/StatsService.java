92ba412f9c861567102e526b4cded25627a8c8ca
Burenkov Oleg <duncanscoffee@gmail.com>
2023-12-06 12:23:36 +0200

    Initial commit

---
+++ src/main/java/ru/netology/QaArrays/service/StatsService.java
@@ -0,0 +1,59 @@
+package ru.netology.QaArrays.service;
+
+public class StatsService {
+    public int minSales(long[] sales) {
+        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
+                minMonth = i; // запомним его как минимальный
+            }
+        }
+        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth + 1;
+    }
+
+    public int getSumSales(long[] sales) {
+        long sumSales = 0;
+        for (long x : sales) {
+            sumSales = sumSales + x;
+        }
+        return (int) sumSales;
+    }
+
+    public int getAverageSumSales(long[] sales) {
+        long averageSumSales = getSumSales(sales);
+        return (int) (averageSumSales / sales.length);
+    }
+
+    public int getLowAverageSumSales(long[] sales) {
+        int lowAverageSumSales = 0;
+        for (long sale : sales) {
+            if (sale < getAverageSumSales(sales)) {
+                lowAverageSumSales++;
+            }
+        }
+        return lowAverageSumSales;
+    }
+
+    public int getHighAverageSumSales(long[] sales) {
+        int highAverageSumSales = 0;
+        for (long sale : sales) {
+            if (sale > getAverageSumSales(sales)) {
+                highAverageSumSales++;
+            }
+        }
+        return highAverageSumSales;
+    }
+
+}
---
+++ src/test/java/ru/netology/QaArrays/service/StatsServiceTest.java
@@ -0,0 +1,70 @@
+package ru.netology.QaArrays.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class StatsServiceTest {
+    long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+    @Test
+
+    public void findMinMonth() {
+        StatsService service = new StatsService();
+
+        int expectedMinMonth = 9;
+        int actualMinMonth = service.minSales(sales);
+
+        Assertions.assertEquals(expectedMinMonth, actualMinMonth);
+    }
+
+    @Test
+    public void findMaxMonth() {
+        StatsService service = new StatsService();
+
+        int expectedMaxMonth = 8;
+        int actualMaxMonth = service.maxSales(sales);
+
+        Assertions.assertEquals(expectedMaxMonth, actualMaxMonth);
+    }
+
+    @Test
+    public void findSumSales() {
+        StatsService service = new StatsService();
+
+        int expectedSum = 180;
+        int actualSum = service.getSumSales(sales);
+
+        Assertions.assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void findAverageSumSales() {
+        StatsService service = new StatsService();
+
+        int expectedAverage = 15;
+        int actualAverage = service.getAverageSumSales(sales);
+
+        Assertions.assertEquals(expectedAverage, actualAverage);
+    }
+
+    @Test
+    public void findLowAverageSumSales() {
+        StatsService service = new StatsService();
+
+        int expectedLow = 5;
+        int actualLow = service.getLowAverageSumSales(sales);
+
+        Assertions.assertEquals(expectedLow, actualLow);
+    }
+
+    @Test
+    public void findHighAverageSumSales() {
+        StatsService service = new StatsService();
+
+        int expectedHigh = 5;
+        int actualHigh = service.getHighAverageSumSales(sales);
+
+        Assertions.assertEquals(expectedHigh, actualHigh);
+    }
+}
